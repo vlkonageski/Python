e7bc026afada483684420670b11666b8dc7381a7
Vinicius Konageski <vinicius_konageski@hotmail.com>
2020-04-22 17:51:45 -0300

    master

--- Exercicios Modulo 5/Exercicio12.py
+++ Exercicios Modulo 5/Exercicio12.py
@@ -2,5 +2,16 @@
 Ler um numero inteiro. Se o numero lido for negativo, escreva a mensagem " numero invalido". Se o numero for positivo.
 Calcular o logaritimo desse numero.
 """
+import math
 
-print("Hello Wolrd")
+n = int(input("Informe um numero inteiro:"))
+
+
+def logaritimo():
+    if n >= 0:
+        print("O logaritimo de", n, "é:", math.log(n))
+    else:
+        print("Numero Invalido!")
+
+
+logaritimo()

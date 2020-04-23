683a22c01e76438044b6da2affc4cd3cf362089e
Vinicius Konageski <vinicius_konageski@hotmail.com>
2020-04-23 16:38:29 -0300

    vlkonageski

--- Exercicios Modulo 5/Exercicio21.py
+++ Exercicios Modulo 5/Exercicio21.py
@@ -7,3 +7,45 @@ Escreva uma mensagem de erro se a opção for invalida.
  4- Divisao entre dois numeros (o denominador nao pode ser zero.)
  Opção
 """
+
+print("1- Soma de dois numeros \n"
+      "2- Diferença entre 2 numeros (maior pelo menor)\n"
+      "3- Produto entre 2 numeros \n"
+      "4- Divisao entre dois numeros (o denominador nao pode ser zero.)")
+
+opcao = int(input("Informe a opção desejada:"))
+    
+
+def calcular():
+    if opcao == 1:
+        n1 = float(input("Informe um numero:"))
+        n2 = float(input("Informe um numero:"))       
+        soma = n1 + n2
+        print(f"A soma de {n1} + {n2} = {soma}")
+    elif opcao == 2:
+        n1 = float(input("Informe um numero:"))
+        n2 = float(input("Informe um numero:"))
+        if n1 > n2:       
+            diferenca = n1 - n2
+            print(f"A diferenca de {n1} - {n2} = {diferenca}")
+        else:
+            diferenca = n2 - n1
+            print(f"A diferenca de {n2} - {n1} = {diferenca}")
+    elif opcao == 3:
+        n1 = float(input("Informe um numero:"))
+        n2 = float(input("Informe um numero:"))       
+        produto = n1 * n2
+        print(f"O produto de {n1} * {n2} = {produto}")
+    elif opcao == 4:
+        n1 = float(input("Informe um numero:"))
+        n2 = float(input("Informe um numero:"))
+        if n2 > 0:       
+            divisao = n1 / n2
+            print(f"A divisao de {n1} / {n2} = {divisao}")
+        else:
+            print("O denominador deve ser maior que zero:")
+    else:
+        print("Opção invalida")
+
+
+calcular()
--- Exercicios Modulo 5/Exercicio22.py
+++ Exercicios Modulo 5/Exercicio22.py
@@ -6,4 +6,19 @@ As condições para aposentadoria sao.
  º Ou ter pelo menos 60 anos e trabalhado pelo menos 25 anos.
 """
 
+idade = int(input("Informe a sua idade:"))
+servico = int(input("Informe quanto tempo de serviço:"))
 
+
+def aposentadoria():
+    if idade >= 65:
+        print("Já pode se aposentar!")
+    elif servico >= 30:
+        print("Já pode se aposentar!")
+    elif idade >= 60 and servico >= 25:
+        print("Já pode se aposentar!")
+    else:
+        print("Não pode se aposentar!")
+
+
+aposentadoria()

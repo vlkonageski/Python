9c0555860ba69ef196dee307ba55afd78595a290
Vinicius Konageski <vinicius_konageski@hotmail.com>
2020-04-23 10:20:13 -0300

    master

--- Exercicios Modulo 5/Exercicio14.py
+++ Exercicios Modulo 5/Exercicio14.py
@@ -1,8 +1,28 @@
 """
-A nota final de um estudante é calculada a partir de tres notas atribuidas entre o intervalo de 0 a 10, respectivamente,
-a um trabalho de laboratorio, a uma avaliação semestral e a um exame final. A media dos três notas mencionadas
-anteriormente obedece aos pesos: Trabalho de Laboratorio: 2; Avaliação semestral: 3; Exame final: 5; De acordo
-com o resultado, mostre na tela se o aluno esta reprovado (media entre 0 e 2,9), de recuperação (entre 3 e 4,9) ou
-se foi aprovado. Faça todas as verificaçlões necessarias.
+A nota final de um estudante é calculada a partir de tres notas atribuidas entre o intervalo de 0 a 10,
+respectivamente, a um trabalho de laboratorio, a uma avaliação semestral e a um exame final. 
+A media dos três notas mencionadas anteriormente obedece aos pesos: Trabalho de Laboratorio: 2; 
+Avaliação semestral: 3; Exame final: 5; De acordo com o resultado, mostre na tela se o aluno 
+esta reprovado (media entre 0 e 2,9), de recuperação (entre 3 e 4,9) ou se foi aprovado. 
+Faça todas as verificaçlões necessarias.
 """
 
+trabalho = float(input("Informe a nota do trabalho de laboratorio:"))
+avaliacao = float(input("Informe a nota da Avaliacao Semestral:"))
+exame = float(input("Informe a nota do Exame Final:"))
+
+
+def media_aluno():
+    media = ((trabalho * 2) + (avaliacao * 3) + (exame * 5)) / 10
+    if media >= 0 and media <= 2.9:
+        print(f"A media do aluno é {media}")
+        print("Aluno Reprovado!")
+    elif media >= 3 and media <= 4.9:
+        print(f"A media do aluno é {media}")
+        print("Aluno de Recuperação!")
+    else:
+        print(f"A media do aluno é {media}")
+        print("Aluno Aprovado!")
+
+
+media_aluno()
--- Exercicios Modulo 5/Exercicio15.py
+++ Exercicios Modulo 5/Exercicio15.py
@@ -1,4 +1,28 @@
 """
-Usando switch, escreva um programa que leia um inteiro entre 1 e 7 e imprima o dia da semana correspondente a este numero.
-Isto é, domingo se 1, segunda-feira se 2, e assim por diante.
+Usando switch, escreva um programa que leia um inteiro entre 1 e 7 e imprima o dia da semana
+correspondente a este numero. Isto é, domingo se 1, segunda-feira se 2, e assim por diante.
 """
+
+n = int(input("Informe um numero de 1 a 7:"))
+
+
+def dias_semana():
+    if n == 1:
+        print("Domingo")
+    elif n == 2:
+        print("Segunda-Feira")
+    elif n == 3:
+        print("Terça-Feira")
+    elif n == 4:
+        print("Quarta-Feira")
+    elif n == 5:
+        print("Quinta-Feira")
+    elif n == 6:
+        print("Sexta-Feira")
+    elif n == 7:
+        print("Sábado")
+    else:
+        print("Numero Invalido")
+
+
+dias_semana()

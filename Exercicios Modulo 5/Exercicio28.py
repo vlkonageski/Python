e03f9980a817e70b525ac541802f1b37297b0f6f
Vinicius Konageski <vinicius_konageski@hotmail.com>
2020-04-23 22:48:12 -0300

    vlkonageski

--- Exercicios Modulo 5/Exercicio28.py
+++ Exercicios Modulo 5/Exercicio28.py
@@ -1,6 +1,6 @@
 """
-Faça um programa que leia tres numeros inteiro positivos e efetuo o calculo de uma das seguintes medias de acordo
-com um valor numerico digitado pelo usuario:
+Faça um programa que leia tres numeros inteiro positivos e efetuo o calculo de uma das seguintes
+medias de acordo com um valor numerico digitado pelo usuario:
 (A) Geometrica: ³raiz X * Y * Z
 (B) Ponderada: X + 2 * Y +3 * Z
                 --------------
@@ -16,4 +16,32 @@ com um valor numerico digitado pelo usuario:
                     3
 """
 
+x = int(input("Informe um numero inteiro:"))
+y = int(input("Informe um numero inteiro:"))
+z = int(input("Informe um numero inteiro:"))
 
+print("(A) Gemometrica \n"
+      "(B) Ponderada \n"
+      "(C) Harmonica \n"
+      "(D) Aritimetica\n")
+opcao = str(input("Escolha uma das opções:"))
+
+
+def medias():
+    if opcao == 'a' or opcao == 'A':
+        geometrica = (x * y * z) ** (1/3)
+        print("A media geometrica é {:.2f}".format(geometrica))
+    elif opcao == 'b' or opcao == 'B':
+        ponderada = (x + 2 * y + 3 * z) / 6
+        print("A media geometrica é {:.2f}".format(ponderada))
+    elif opcao == 'c' or opcao == 'C':
+        harmonica = 1 / (1 / x + 1 / y + 1 / z)
+        print("A media geometrica é {:.2f}".format(harmonica))
+    elif opcao == 'd' or opcao == 'D':
+        aritimetica = (x + y + z) / 3
+        print("A media geometrica é {:.2f}".format(aritimetica))
+    else:
+        print("Opção Invalida!")
+
+
+medias()
--- Exercicios Modulo 5/Exercicio29.py
+++ Exercicios Modulo 5/Exercicio29.py
@@ -1,7 +1,8 @@
 """
-Faça uma prova de matematica para crianças que estao aprendendo a somar numeros inteiro menores do que 100.
-Escolha numeros aleatorios entre 1 e 100, e mostre na tela a pergunta:
-qual é a soma de A + , onde A e B sao os numeros aleatorios. Peça a resposta. Faça cinco perguntas ao aluno e
-mostre para ele as perguntas e as respostas corretas, alem de quantas vezes o aluno acertou.
+Faça uma prova de matematica para crianças que estao aprendendo a somar numeros inteiro menores
+do que 100. Escolha numeros aleatorios entre 1 e 100, e mostre na tela a pergunta:
+qual é a soma de A + , onde A e B sao os numeros aleatorios. Peça a resposta. 
+Faça cinco perguntas ao aluno emostre para ele as perguntas e as respostas corretas,
+alem de quantas vezes o aluno acertou.
 """
 

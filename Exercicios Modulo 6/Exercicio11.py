a322553b07fe86e50d44fe73442b53270a16deec
Vinicius Konageski <vinicius_konageski@hotmail.com>
2020-05-03 17:29:38 -0300

    vlkonageski

--- Exercicios Modulo 6/Exercicio11.py
+++ Exercicios Modulo 6/Exercicio11.py
@@ -1,5 +1,6 @@
 """
-Faça um programa que leia um numero inteiro positivo N e imprima todos os numeros naturais de 0 ate N em ordem crescente.
+Faça um programa que leia um numero inteiro positivo N e imprima todos os numeros naturais de 0 ate N
+em ordem crescente.
 """
 
 

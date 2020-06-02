82e9bba19699ff1b5db4ddf42617715f02068d04
Vinicius Konageski <vinicius@alimentoscenci.com>
2020-06-02 17:05:43 -0300

    Resolucao exercicios modulo 6 de 31 a 33

--- Exercicios Modulo 6/Exercicio31.py
+++ Exercicios Modulo 6/Exercicio31.py
@@ -4,3 +4,16 @@ Faça um programa que calcule e escreva o valor de S
         S= --  --  --  -- ... --
             1   2   3   4     50
 """
+
+s1 = 0
+s2 = 0
+
+for i in range(1, 50):
+    if i % 2 != 0:
+        s1 += 1
+        s2 += i
+resultado = s2 / s1
+
+print(s2)
+print(s1)
+print(resultado)
--- Exercicios Modulo 6/Exercicio32.py
+++ Exercicios Modulo 6/Exercicio32.py
@@ -2,3 +2,17 @@
 Faça um programa que simula o lançamento de dois dados, D1 e D2, N vezes, e tem como saída o número de cada dado
 e a relação entre eles (>,<,=) de cada lançamento.
 """
+from random import randint
+
+n = int(input('Informe quantas vezes quer jogar os dados:'))
+
+
+for i in range(1, n+1):
+    d1 = int(randint(1, 6))
+    d2 = int(randint(1, 6))
+    if d1 > d2:
+        print('{} > {}'.format(d1, d2))
+    elif d1 == d2:
+        print('{} = {}'.format(d1, d2))
+    else:
+        print('{} < {}'.format(d1, d2))

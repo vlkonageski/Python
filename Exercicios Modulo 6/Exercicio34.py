186bd799d74412aa920ab72a281a7041b528089d
vlkonageski <30873397+vlkonageski@users.noreply.github.com>
2020-06-12 10:08:13 -0300

    Update Exercicio34.py
    Resolução erxercicio 34

--- Exercicios Modulo 6/Exercicio34.py
+++ Exercicios Modulo 6/Exercicio34.py
@@ -2,3 +2,14 @@
 Faça um programa que calcule o menor numero divisivel por cada um dos numeros de 1 a 20?
 Ex: 2520 é o menor numero que pode ser dividido por cada um dos numeros de 1 a 10, sem sobrar resto.
 """
+
+# importar o gcd da biblioteca math, ele calcula o mmc entre dois valores diferentes de zero.
+from math import gcd
+
+numeros = list(range(1, 21))
+m = 1
+
+for n in numeros:
+    m = m * n // gcd(m, n)
+
+print(m)

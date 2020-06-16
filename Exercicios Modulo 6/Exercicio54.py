9de41e53d4cc1cfcc336e5db247526509ad403c1
vlkonageski <vinicius_konageski@hotmail.com>
2020-06-16 20:51:39 -0300

    ajustes

--- Exercicios Modulo 6/Exercicio54.py
+++
@@ -1,7 +0,0 @@
-"""
-Faça um programa que receba um numero inteiro maior do que 1, e verifique se o numero fornecido é primo ou nao.
-"""
-
-n = int(input('Informe um numero maior que 1: '))
-x = 0
-

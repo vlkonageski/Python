9279bd38dea9788caffd75940b0cb93a645de326
vlkonageski <vinicius_konageski@hotmail.com>
2020-06-15 22:24:37 -0300

    vlkonageski

--- Exercicios Modulo 6/Exercicio47.py
+++ Exercicios Modulo 6/Exercicio47.py
@@ -10,3 +10,38 @@ O programa só termina quando for escolhida a opção de saída (opção 5).
 """
 
 
+while True:
+    print('Menu de Opcoes\n'
+    '1 - Adicao\n'
+    '2 - Subtracao\n'
+    '3 - Multiplicacao\n'
+    '4 - Divisao\n'
+    '5 - Sair')
+    opcao = int(input('Informe a opcao desejada:'))
+    
+    if opcao == 1:
+        n1 = int(input('Informe o primeiro valor:'))
+        n2 = int(input('Informe o segundo valor:'))
+        soma = n1 + n2
+        print('A Soma de {} + {} = {}'.format(n1, n2, soma))
+    elif opcao == 2:
+        n1 = int(input('Informe o primeiro valor:'))
+        n2 = int(input('Informe o segundo valor:'))
+        subtracao = n1 - n2
+        print('A Subtracao de {} - {} = {}'.format(n1, n2, subtracao))
+    elif opcao == 3:
+        n1 = int(input('Informe o primeiro valor:'))
+        n2 = int(input('Informe o segundo valor:'))
+        multiplicacao = n1 * n2
+        print('A Multiplicacao de {} * {} = {}'.format(n1, n2, multiplicacao))
+    elif opcao == 4:
+        n1 = int(input('Informe o primeiro valor:'))
+        n2 = int(input('Informe o segundo valor:'))
+        divisao = n1 / n2
+        print('A Divisao de {} / {} = {}'.format(n1, n2, divisao))
+    elif opcao == 5:
+        print('Programa Finalizado!')
+        break
+    else:
+        print('Opcao Invalida!')
+    
